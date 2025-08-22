2729780f0bf7d8782c99ae149d526c480b5f48cc
Thorsten Altenkirch <psztxa@exmail.nottingham.ac.uk>
2025-08-22 15:17:39 +0100

    Initial commit

---
+++ Content/Intro.lean
@@ -0,0 +1,16 @@
+import VersoManual
+import Content.Meta.Lean
+open Verso.Genre Manual
+open Verso.Genre.Manual.InlineLean
+
+open Content
+--set_option pp.rawOnError true
+
+#doc (Manual) "Introduction" =>
+
+These are lecture notes for COMP2065: Introduction to formal
+reasoning. The main goal is to teach
+formal logic using an interactive proof system called **Lean**. You will be
+able to use predicate logic to make precise statements and to verify
+them using a proof system. The covers both statements in Mathematics
+and statements about computer programs, e.g. their correctness.
---
+++ IFRNotes.lean
@@ -0,0 +1,16 @@
+import VersoManual
+import Content.Meta.Lean
+open Verso.Genre Manual
+open Verso.Genre.Manual.InlineLean
+
+open Content
+set_option pp.rawOnError true
+
+
+#doc (Manual) "IFR (COMP2065) lecture notes" =>
+
+%%%
+authors := ["Thorsten Altenkirch"]
+%%%
+
+include Intro
