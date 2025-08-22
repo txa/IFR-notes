7e4d153f52e27bb2cab69ac34c79f2e4db68cc39
Thorsten Altenkirch <psztxa@exmail.nottingham.ac.uk>
2025-08-22 17:00:36 +0100

    import ?

--- IFRNotes.lean
+++ IFRNotes.lean
@@ -1,4 +1,7 @@
 import VersoManual
+
+--import Content.Intro
+
 --import Content.Meta.Lean
 open Verso.Genre Manual
 open Verso.Genre.Manual.InlineLean
@@ -6,11 +9,11 @@ open Verso.Genre.Manual.InlineLean
 --open Content
 set_option pp.rawOnError true
 
-
+-- {include 1 Content.Intro}
 #doc (Manual) "IFR (COMP2065) lecture notes" =>
 
 %%%
 authors := ["Thorsten Altenkirch"]
 %%%
 
-include Content.Intro
+bla
